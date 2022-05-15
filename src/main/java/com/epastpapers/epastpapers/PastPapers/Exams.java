c71736f781e8f75e905c4b0a2eb8fbf1f5ceacd8
mainalito <81359755+mainalito@users.noreply.github.com>
2022-05-15 11:36:17 +0300

    final year project

---
+++ src/main/java/com/epastpapers/epastpapers/PastPapers/Exams.java
@@ -0,0 +1,52 @@
+package com.epastpapers.epastpapers.PastPapers;
+
+import java.util.Arrays;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Exams {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 128, nullable = false)
+	private String fileName;
+
+	private String originalFileName;
+	private String fileType;
+
+	@Lob
+	private byte[] data; //store file as an array in database
+
+	private int year;
+
+	@ManyToOne
+	@JoinColumn(name = "faculty_id")
+	private FACULTY faculty;
+
+	public Exams( String fileName, String fileType, byte[] data, int year, FACULTY faculty) {
+		this.fileName = fileName;
+		this.fileType = fileType;
+		this.data = data;
+		this.year = year;
+		this.faculty = faculty;
+	}
+
+
+}
